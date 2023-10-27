750ba21ece4897d9ec1cbc9ef926c8a731a1e44d
Mateo Cordoba <matucorneron@gmail.com>
2023-10-27 16:20:04 -0300

    Actualizar compileSDK a version 34

--- CalendarAppIDRA/app/build.gradle.kts
+++ CalendarAppIDRA/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.calendarapp_idra"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.calendarapp_idra"
