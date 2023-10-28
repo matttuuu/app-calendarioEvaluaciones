0f79856968316c4e2e727a5d665b09ae100e1757
Mateo Cordoba <matucorneron@gmail.com>
2023-10-27 22:54:43 -0300

    Primer version de nav drawer - pending onclick

---
+++ CalendarAppIDRA/app/src/main/java/Fragments/MainFragment.java
@@ -0,0 +1,4 @@
+package Fragments;
+
+public class MainFragment {
+}
--- CalendarAppIDRA/app/src/main/java/com/example/calendarapp_idra/MainActivity.java
+++ CalendarAppIDRA/app/src/main/java/com/example/calendarapp_idra/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.calendarapp_idra;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar tooolbar;
+    NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tooolbar = findViewById(R.id.toolbar); //Referenciamos la variable de objeto toolbar al que tenemos en la vista main
+        setSupportActionBar(tooolbar);
+        drawerLayout = findViewById(R.id.drawer); //Realizamos lo mismo :)
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,tooolbar,R.string.open,R.string.close); ///*BORRAR: Explicado" en minuto 17:35
+        drawerLayout.addDrawerListener(actionBarDrawerToggle); //Le añadimos un listener al drawerLayout
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
     }
 }
