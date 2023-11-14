5ef7facf534c0169cc20b252ba55d66044565ebe
Laureano <laureano.orr@hotmail.com>
2023-11-14 05:20:14 -0300

    Agregar show_events_rowlayout.xml y la clase EventRecycleAdapter

--- CalendarAppIDRA/app/src/main/java/com/example/calendarapp_idra/CustomCalendarView.java
+++ CalendarAppIDRA/app/src/main/java/com/example/calendarapp_idra/CustomCalendarView.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -19,6 +20,8 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -39,6 +42,7 @@ public class CustomCalendarView extends LinearLayout {
     SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
     SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH);
     SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+    SimpleDateFormat eventDateFormate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
 
 
 
@@ -79,7 +83,7 @@ public class CustomCalendarView extends LinearLayout {
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 builder.setCancelable(true);
                 View addView = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_newevent_layout, null);
-                EditText EventName = addView.findViewById(R.id.events_id);
+                EditText EventName = addView.findViewById(R.id.eventname);
                 TextView EventTime = addView.findViewById(R.id.eventtime);
                 ImageButton SetTime = addView.findViewById(R.id.seteventtime);
                 Button AddEvent = addView.findViewById(R.id.addevent);
@@ -106,9 +110,9 @@ public class CustomCalendarView extends LinearLayout {
 
                     }
                 });
-                String date = dateFormat.format(dates.get(position));
-                String month = monthFormat.format(dates.get(position));
-                String year = yearFormat.format(dates.get(position));
+                final String date = eventDateFormate.format(dates.get(position));
+                final String month = monthFormat.format(dates.get(position));
+                final String year = yearFormat.format(dates.get(position));
 
                 AddEvent.setOnClickListener(new OnClickListener() {
                     @Override
@@ -126,10 +130,59 @@ public class CustomCalendarView extends LinearLayout {
             }
         });
 
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @SuppressLint("MissingInflatedId") //AGREGADO POR MI, EN CASO DE QUE NO ANDE, BORRAR Y BUSCAR OTRA SOLUCIÓN.
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String date = eventDateFormate.format(dates.get(position));
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setCancelable(true);
+                View showView = LayoutInflater.from(parent.getContext()).inflate(R.layout.show_events_layout, null);
+                RecyclerView recyclerView = showView.findViewById(R.id.EventsRV);
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(showView.getContext());
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setHasFixedSize(true);
+                EventRecyclerAdapter eventRecyclerAdapter = new EventRecyclerAdapter(showView.getContext(),
+                        CollectEventByDate(date));
+                recyclerView.setAdapter(eventRecyclerAdapter);
+                eventRecyclerAdapter.notifyDataSetChanged();
+
+                builder.setView(showView);
+                alertDialog = builder.create();
+                alertDialog.show();
+
+
+                return true;
+            }
+        });
+
 
 
 
     }
+
+    private ArrayList<Events> CollectEventByDate(String date) {
+        ArrayList<Events> arrayList = new ArrayList<>();
+        DBOpenHelper dbOpenHelper = new DBOpenHelper(context); //DBOPENHELPER TAMBIÉN PUESTO, NO SÉ SI ESTA BIEN.
+        SQLiteDatabase database = dbOpenHelper.getReadableDatabase();
+        Cursor cursor = dbOpenHelper.ReadEvents(date, database);
+        while (cursor.moveToNext()) {
+            @SuppressLint("Range") String event = cursor.getString(cursor.getColumnIndex(DBStructure.EVENT)); //LOS SUPRESSLINT SON INVENCIÓN MÍA, EN CASO DE ERROR AL USAR ESTE MÉTODO, BORRALO BEBEEE
+            @SuppressLint("Range") String time = cursor.getString(cursor.getColumnIndex(DBStructure.TIME));
+            @SuppressLint("Range") String Date = cursor.getString(cursor.getColumnIndex(DBStructure.DATE));
+            @SuppressLint("Range") String month = cursor.getString(cursor.getColumnIndex(DBStructure.MONTH));
+            @SuppressLint("Range") String Year = cursor.getString(cursor.getColumnIndex(DBStructure.YEAR));
+            Events events = new Events(event, time, Date, month, Year);
+            arrayList.add(events);
+
+        }
+        cursor.close();
+        dbOpenHelper.close();
+
+        return arrayList;
+    }
+
 
 
     public CustomCalendarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -138,8 +191,8 @@ public class CustomCalendarView extends LinearLayout {
 
     private void SaveEvent (String event, String time, String date, String month, String year){
 
-        DBOpenHelper // SI NO, NO ANDA LA VARIABLE dbOpenHelper, revisar despues.
-        dbOpenHelper = new DBOpenHelper(context);
+
+        DBOpenHelper dbOpenHelper = new DBOpenHelper(context); // SI NO, NO ANDA LA VARIABLE dbOpenHelper, revisar despues.
         SQLiteDatabase database = dbOpenHelper.getWritableDatabase();
         dbOpenHelper.SaveEvent(event, time, date, month, year, database);
         dbOpenHelper.close();
@@ -166,6 +219,7 @@ public class CustomCalendarView extends LinearLayout {
         monthCalendar.set(Calendar.DAY_OF_MONTH, 1);
         int FirstDayofMonth = monthCalendar.get(Calendar.DAY_OF_WEEK)-1;
         monthCalendar.add(Calendar.DAY_OF_MONTH, -FirstDayofMonth);
+        CollectEventsPerMonth(monthFormat.format(calendar.getTime()), yearFormat.format(calendar.getTime()));
 
         while (dates.size() < MAX_CALENDAR_DAYS) {
         dates.add(monthCalendar.getTime());
@@ -178,4 +232,30 @@ public class CustomCalendarView extends LinearLayout {
 
 
     }
+
+    private void CollectEventsPerMonth(String Month, String year){
+
+
+        eventsList.clear();
+        DBOpenHelper dbOpenHelper = new DBOpenHelper(context); //REVISAR DESPUÉS PROBLEMA CON DB
+        SQLiteDatabase database = dbOpenHelper.getReadableDatabase();
+        Cursor cursor = dbOpenHelper.ReadEventsperMonth(Month, year, database);
+
+        while (cursor.moveToNext()) {
+            @SuppressLint("Range") String event = cursor.getString(cursor.getColumnIndex(DBStructure.EVENT)); //LOS SUPRESSLINT SON INVENCIÓN MÍA, EN CASO DE ERROR AL USAR ESTE MÉTODO, BORRALO BEBEEE
+            @SuppressLint("Range") String time = cursor.getString(cursor.getColumnIndex(DBStructure.TIME));
+            @SuppressLint("Range") String date = cursor.getString(cursor.getColumnIndex(DBStructure.DATE));
+            @SuppressLint("Range") String month = cursor.getString(cursor.getColumnIndex(DBStructure.MONTH));
+            @SuppressLint("Range") String Year = cursor.getString(cursor.getColumnIndex(DBStructure.YEAR));
+            Events events = new Events(event, time, date, month, Year);
+            eventsList.add(events);
+
+        }
+        cursor.close();
+        dbOpenHelper.close();
+
+    }
+
+
+
 }
